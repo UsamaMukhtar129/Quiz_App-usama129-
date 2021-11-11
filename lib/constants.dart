20ae39d6b1ebb8abe8e095281fa47223de7716bd
Usama Mukhtar(FA18-BCS-129) <92028293+UsamaMukhtar129@users.noreply.github.com>
2021-11-10 21:20:10 -0800

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF8B9CB4);
+const kGreenColor = Color(0xFF6AC6AC);
+const kRedColor = Color(0xEEE85F90);
+const kGrayColor = Color(0xFFC1C1C1);
+const kBlackColor = Color(0xFF101010);
+const kPrimaryGradient = LinearGradient(
+  colors: [Color(0xFF46EA), Color(0xAF99FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const double kDefaultPadding = 20.0;
